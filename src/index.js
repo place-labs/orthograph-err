c1d5db076660f0f52e3134970d743fe6988029b5
Kim Burgess <kim@place.technology>
2020-05-07 22:11:31 +1000

    base textlint parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const glob = require('@actions/glob');
+const TextLintEngine = require("textlint").TextLintEngine;
+
+async function getFiles() {
+  let path = core.getInput('path', { required: true });
+  let globber = await glob.create(path);
+  return globber.glob();
+}
+
+async function lint(files) {
+  let engine = new TextLintEngine();
+  let results = await engine.executeOnFiles(files);
+  if (engine.isErrorResults(results)) {
+    let output = engine.formatResults(results);
+    core.setFailed(output);
+  }
+}
 
 try {
-  const payload = JSON.stringify(github.context.payload);
-  console.log(`The event payload: ${payload}`);
+  let files = getFiles();
+  lint(files);
 } catch (error) {
   core.setFailed(error.message);
 }

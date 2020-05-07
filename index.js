a8b5209c6cda58948cbb85e6c5d492be67d6e924
Kim Burgess <kim@place.technology>
2020-05-08 03:13:52 +1000

    simplify check updates

--- index.js
+++ index.js
@@ -12,33 +12,28 @@ async function lint(path) {
   return results;
 }
 
-function annotate(api, annotations) {
-  api.checks.update({
-    ...github.context.repo,
-    check_run_id: process.env.GITHUB_RUN_ID,
-    output: {
-      title: 'Textlint',
-      summary: 'Linter results',
-      annotations: annotations,
-    }
-  });
-}
-
 function updateChecks(api, results) {
   const level = ['notice', 'warning', 'failure'];
-  results.map((result) => {
-    annotate(api, result.messages.map((message) => {
-      ({
-        path:             result.filePath,
-        start_line:       message.line,
-        end_line:         message.line,
-        start_column:     message.column,
-        end_column:       message.column,
-        annotation_level: level[message.severity],
-        message:          message.message,
-        title:            message.ruleId,
-      })
-    }));
+
+  results.map(result => {
+    api.checks.update({
+      ...github.context.repo,
+      check_run_id: process.env.GITHUB_RUN_ID,
+      output: {
+        title: 'Textlint',
+        summary: 'Linter results',
+        annotations: result.messages.map(message => ({
+          path:             result.filePath,
+          start_line:       message.line,
+          end_line:         message.line,
+          start_column:     message.column,
+          end_column:       message.column,
+          annotation_level: level[message.severity],
+          message:          message.message,
+          title:            message.ruleId,
+        })),
+      },
+    })
   });
 }
 

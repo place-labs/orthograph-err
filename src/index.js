45e6eb1ad59c646d91640b34074e3d1094c287bf
Kim Burgess <kim@place.technology>
2020-11-24 20:21:48 +1000

    fix checks target and neaten debug output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const { context }   = require('@actions/github');
+const { context } = require('@actions/github');
 
 const annotate = require('./annotate');
 const linter   = require('./linter');
@@ -7,19 +7,29 @@ const { path } = require('./inputs');
 
 async function run() {
   try {
-    core.startGroup('Running linter');
-    core.info(`Checking files in ${path}`);
-    const results  = await linter.run(path);
-    core.info('Done');
-    core.debug(results);
-    core.endGroup();
+    const results = await core.group('Running linter', async () => {
+      core.info(`Checking files matching ${path}`);
 
-    core.startGroup('Annotating results');
-    core.info(`Adding inline annotations to ${context.repo.repo}#${context.sha}`);
-    const response = await annotate(context.repo, context.sha, results);
-    core.info('Done');
-    core.debug(response);
-    core.endGroup();
+      const results = await linter.run(path);
+
+      const output = linter.engine.formatResults(results);
+      core.info(output);
+
+      return results;
+    });
+
+    await core.group('Applying annotations', async () => {
+      let sha = context.sha
+      // PR's require check status to be linked to head commit SHA, whereas
+      // context.sha refers to the associated merge commit.
+      if (context.eventName == 'pull_request') {
+        sha = context.payload.pull_request.head.sha
+      }
+      core.info(`Adding inline annotations to ${context.repo.repo}@${sha}`);
+
+      const response = await annotate(context.repo, sha, results);
+      core.debug(JSON.stringify(response, null, 2));
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
